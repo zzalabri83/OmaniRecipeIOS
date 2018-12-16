cf5cb75c9505494f8ce46fa85832a1c767bdaab7
Zahran AlAbri ITA <zzalabri83@gmail.com>
2018-12-16 16:12:29 +0400

    edit 1 for test propose

--- OmaniRecipeV1.1/OmaniRecipeV1.1/SignInViewController.swift
+++ OmaniRecipeV1.1/OmaniRecipeV1.1/SignInViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class SignInViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var txtEmail: UITextField!
     
