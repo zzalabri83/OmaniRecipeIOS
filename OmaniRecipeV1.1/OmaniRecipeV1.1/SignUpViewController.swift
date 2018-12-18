7820c335b73f044aee9884da51a17cf0186c716c
Zahran AlAbri ITA <zzalabri83@gmail.com>
2018-12-18 11:20:40 +0400

    a

--- OmaniRecipeV1.1/OmaniRecipeV1.1/SignUpViewController.swift
+++ OmaniRecipeV1.1/OmaniRecipeV1.1/SignUpViewController.swift
@@ -21,7 +21,6 @@ class SignUpViewController: UIViewController {
     fileprivate func showAlert(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        
         alertController.addAction(defaultAction)
         self.present(alertController, animated: true, completion: nil)
     }
